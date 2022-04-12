800e2d8baf9816077c1f07c888648d213745ba09
fdurante <fdurante@alu.ucam.edu>
2022-04-12 16:57:44 +0200

    buscar por país

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -10,10 +11,16 @@ export class PorPaisComponent {
 
   termino: string = '';
 
-  constructor() { }
+  constructor( private PaisService: PaisService ) { }
 
   buscar() {
+
     console.log(this.termino);
+    this.PaisService.buscarPais( this.termino )
+      .subscribe( resp => {
+        console.log( resp );
+      });
+
   }
 
 }
