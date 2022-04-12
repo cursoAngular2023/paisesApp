48a2173e701613720e79c2e157c18e23d6f4c1e2
fdurante <fdurante@alu.ucam.edu>
2022-04-12 17:12:56 +0200

    manejo errores

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -10,17 +10,24 @@ import { PaisService } from '../../services/pais.service';
 export class PorPaisComponent {
 
   termino: string = '';
+  hayError: boolean = false;
 
   constructor( private PaisService: PaisService ) { }
 
   buscar() {
 
+    this.hayError = false;
     console.log(this.termino);
     this.PaisService.buscarPais( this.termino )
-      .subscribe( resp => {
-        console.log( resp );
-      });
-
+      .subscribe({
+        next: (data) => {
+          console.log( data);
+      },
+       error: (err) => {
+        console.log('Error');
+        console.info(err);
+        this.hayError = true;
+      }
+    });
   }
-
 }
